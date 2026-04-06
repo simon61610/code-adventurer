ec52e4775ced3351ad9dfb58d64a73537960881e
simon61610 <simon410379007@gmail.com>
2026-04-06 18:53:50 +0800

    feat(style): 調整 AppHeader 樣式與結構

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,29 +1,62 @@
 <script setup lang="ts">
+import logo from '@/assets/images/logo.png'
 </script>
 
 <template>
   <header class="header">
-    <div class="logo">MyLogo</div>
+    <!-- 左邊 -->
+    <nav class="nav nav-left">
+      <router-link to="/">關於</router-link>
+      <router-link to="/portfolio">任務</router-link>
+    </nav>
 
-    <nav class="nav">
-      <router-link to="/">首頁</router-link>
-      <router-link to="/about">關於</router-link>
+    <!-- 中間 Logo -->
+    <router-link to="/" class="logo">
+      <img :src="logo" alt="logo" />
+    </router-link>
+
+    <!-- 右邊 -->
+    <nav class="nav nav-right">
+      <router-link to="/blog">魔王</router-link>
+      <router-link to="/contact">地圖</router-link>
     </nav>
   </header>
 </template>
 
 <style scoped lang="scss">
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 16px;
-  background: #222;
-  color: white;
-}
+  outline: 1px solid red;
+  padding: 0 40px;
 
-.nav {
-  display: flex;
-  gap: 16px;
+  // 三欄版型：左 / 中 / 右 -> 1fr = 平分剩餘空間 | auto = 依內容寬度
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  align-items: center;
+  gap: 40px;
+
+  // nav
+  .nav {
+    display: flex;
+    gap: 60px;
+    font-size: 20px;
+  }
+
+  .nav-left {
+    justify-self: end;
+  }
+
+  .nav-right {
+    justify-self: start;
+  }
+
+  // logo
+  .logo {
+    justify-self: center;
+
+    img {
+      height: 200px;
+      display: block;
+    }
+  }
 }
 </style>
