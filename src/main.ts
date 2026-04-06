a9695f7495a4fd13300bf7ed6a423e97566b6a25
simon61610 <simon410379007@gmail.com>
2026-04-06 18:03:16 +0800

    feat(style): 引入全域 SASS 樣式 main.scss

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/styles/main.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
-
-/* import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app') */
