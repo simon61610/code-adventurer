0c72bafbd664831acda33bca090619ca5c3ee003
simon61610 <simon410379007@gmail.com>
2026-04-06 19:07:16 +0800

    feat(style): 在 Vite 設定中加入全域 SCSS additionalData

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,21 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/abstracts/variables" as *;
+          @use "@/assets/styles/abstracts/mixin" as *;
+          @use "@/assets/styles/abstracts/functions" as *;
+        `,
+      },
     },
   },
 })
